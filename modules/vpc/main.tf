c4d25f7210894108ae2a159e29678db7b5c1771f
sd <ab@cd.com>
2026-05-30 22:39:41 +0545

    update: separate sn,rt

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -16,68 +16,68 @@ resource "aws_internet_gateway" "sd-gw" {
   tags   = { Name = "${var.target_env}-igw" }
 }
 
-# Create public subnets
-resource "aws_subnet" "public" {
-  count                   = 2
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.public_subnet[count.index]
-  availability_zone       = var.available_zone[count.index]
-  map_public_ip_on_launch = true
-  tags                    = { Name = "${var.target_env}-public-subnet-${count.index + 1}" }
-}
+# # Create public subnets
+# resource "aws_subnet" "public" {
+#   count                   = 2
+#   vpc_id                  = aws_vpc.main.id
+#   cidr_block              = var.public_subnet[count.index]
+#   availability_zone       = var.available_zone[count.index]
+#   map_public_ip_on_launch = true
+#   tags                    = { Name = "${var.target_env}-public-subnet-${count.index + 1}" }
+# }
 
-# Create private subnets in the same availability zones as the public subnets
-resource "aws_subnet" "private" {
-  count                   = 2
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.private_subnet[count.index]
-  availability_zone       = var.available_zone[count.index]
-  map_public_ip_on_launch = false
-  tags                    = { Name = "${var.target_env}-private-subnet-${count.index + 1}" }
-}
+# # Create private subnets in the same availability zones as the public subnets
+# resource "aws_subnet" "private" {
+#   count                   = 2
+#   vpc_id                  = aws_vpc.main.id
+#   cidr_block              = var.private_subnet[count.index]
+#   availability_zone       = var.available_zone[count.index]
+#   map_public_ip_on_launch = false
+#   tags                    = { Name = "${var.target_env}-private-subnet-${count.index + 1}" }
+# }
 
-# Create a DB subnet group for RDS using the private subnets
-resource "aws_db_subnet_group" "rds" {
-  name       = "${var.target_env}-rds-subnet-group"
-  subnet_ids = aws_subnet.private[*].id # Uses your private subnet IDs automatically
+# # Create a DB subnet group for RDS using the private subnets
+# resource "aws_db_subnet_group" "rds" {
+#   name       = "${var.target_env}-rds-subnet-group"
+#   subnet_ids = aws_subnet.private[*].id # Uses your private subnet IDs automatically
 
-  tags = { Name = "${var.target_env}-rds-subnet-group" }
-}
+#   tags = { Name = "${var.target_env}-rds-subnet-group" }
+# }
 
-# Create a route table for public subnets
-resource "aws_route_table" "public-rt" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.sd-gw.id
-  }
-  tags = { Name = "${var.target_env}-public-rt" }
-}
+# # Create a route table for public subnets
+# resource "aws_route_table" "public-rt" {
+#   vpc_id = aws_vpc.main.id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.sd-gw.id
+#   }
+#   tags = { Name = "${var.target_env}-public-rt" }
+# }
 
-resource "aws_route_table_association" "public" {
-  count          = 2
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public-rt.id
-}
+# resource "aws_route_table_association" "public" {
+#   count          = 2
+#   subnet_id      = aws_subnet.public[count.index].id
+#   route_table_id = aws_route_table.public-rt.id
+# }
 
-# aws route table - private
-resource "aws_route_table" "private-rt" {
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "${var.target_env}-private-rt"
-  }
-}
+# # aws route table - private
+# resource "aws_route_table" "private-rt" {
+#   vpc_id = aws_vpc.main.id
+#   tags = {
+#     Name = "${var.target_env}-private-rt"
+#   }
+# }
 
-resource "aws_route_table_association" "private" {
-  count          = 2
-  subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private-rt.id
-}
+# resource "aws_route_table_association" "private" {
+#   count          = 2
+#   subnet_id      = aws_subnet.private[count.index].id
+#   route_table_id = aws_route_table.private-rt.id
+# }
 
-resource "aws_vpc_endpoint" "s3" {
-  vpc_id            = aws_vpc.main.id
-  service_name      = "com.amazonaws.${var.aws_region}.s3"
-  vpc_endpoint_type = "Gateway"
-  route_table_ids   = [aws_route_table.private-rt.id]
-  tags              = { Name = "${var.target_env}-s3-endpoint" }
-}
+# resource "aws_vpc_endpoint" "s3" {
+#   vpc_id            = aws_vpc.main.id
+#   service_name      = "com.amazonaws.${var.aws_region}.s3"
+#   vpc_endpoint_type = "Gateway"
+#   route_table_ids   = [aws_route_table.private-rt.id]
+#   tags              = { Name = "${var.target_env}-s3-endpoint" }
+# }

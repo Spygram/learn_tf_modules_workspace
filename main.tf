3e29979906412cc9cab0782638d5aa5e232f22d5
sd <ab@cd.com>
2026-05-30 17:45:46 +0545

    update: ci.yaml secrets reconfigure

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 module "vpc" {
   source = "./modules/vpc"
 
-  target_env     = terraform.workspace
+  target_env = terraform.workspace
 
-  aws_region           = var.aws_region     # This variable is defined in variables.tf
+  aws_region = var.aws_region # This variable is defined in variables.tf
 
-  vpc_cidr             = var.vpc_cidr       # This variable is defined in variables.tf
+  vpc_cidr = var.vpc_cidr # This variable is defined in variables.tf
 
-  available_zone       = data.aws_availability_zones.available.names # This data source is defined in data.tf
-  
-  public_subnet  = var.public_subnet_cidrs # This variable is defined in variables.tf
-  
+  available_zone = data.aws_availability_zones.available.names # This data source is defined in data.tf
+
+  public_subnet = var.public_subnet_cidrs # This variable is defined in variables.tf
+
   private_subnet = var.private_subnet_cidrs # This variable is defined in variables.tf
-  
+
 }
